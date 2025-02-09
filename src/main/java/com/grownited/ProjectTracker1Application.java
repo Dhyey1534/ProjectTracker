1ae36f5751d23b1d161661bad8fe1a6fc18db307
Dhyey1534 <dhyeypatel152004@gmail.com>
2025-02-09 18:57:35 +0530

    init

---
+++ src/main/java/com/grownited/ProjectTracker1Application.java
@@ -0,0 +1,13 @@
+package com.grownited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTracker1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTracker1Application.class, args);
+	}
+
+}
